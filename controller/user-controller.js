66e6a82c185736ad91932e0ac821ca151f2f4ba2
Jay Idrees <idreesjay@gmail.com>
2021-06-23 03:44:25 -0400

    Added user controller code

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,40 +1,87 @@
-const validateResult=require('express-validator');
+const {validationResult}=require('express-validator');
 
 const httpError=require('./model/http-error');
-const User=require('./model/user')
+const User=require('./model/user');
 
+// Get users
 
-
-// Get Request
-
-const getUsers=async(req,res,next)=>{
+const getUsers=async (req,res,next)=>{
     let users;
     try{
-        users= await User.find({},'-password');
-    } catch(err){
-       const error=new httpError('Unable to fetch users', 500);
-        return next(error)
-    };
-    res.json({users:users.map(user=>user.toObject({getters:true}))})
-}
+        users=await User.find({},'-password');
+    }catch(err){
+        const error=httpError('An error occurred while obtainng the users',422);
+        return next(error);
+    }
+    res.json({users:users.map(user=>user.toObject({getters:true}))});
+};
 
 
-//Sign up
 const signup=async(req,res,next)=>{
     const errors=validationResult(req);
-    if (errors.isEmpty){
-        return next(new httpError("Inputs passed contain errors",422));
+    if(!errors.isEmpty()){
+        return next(new httpError('The information you input did not pass validation checks, please try again', 422))
     }
-    const {name, email, password}=req.body;
 
-    let existinguser
+    // Find whether an emmail already exisits-existing user
+    const {name, email, password}=req.body
+
+    let existingUser;
     try{
-        existinguser=await User.findOne({email:email})
+        existingUser=await User.findOne({email:email});
+    } catch(err){
+        const error=new httpError('An error occurred while signing up, please try again', 422);
+        return next(error);
     }
-}// end of signup
+
+    if (existingUser){
+        const error=new httpError("The email you provided already exists, please log in",500)
+        return next(error);
+    }
+
+    // Capture the signup data into created user object
+
+    const createdUser=new User ({
+        email,
+        password,
+        name,
+        image:'www.electives.us',
+        places:[]
+    });
 
 
+    // Save the information
+
+    try{
+       
+            await createdUser.save();
+        
+    }catch(err){
+        const error=new httpError('Signing up failed, please try again later', 404);
+        return next(error);
+    }
 
 
+    // Send response
+    res.status(201).json({user:createdUser.toObject({getters:true})});
 
-// Login
+}
+
+const login=async (req,res,next)=>{
+const {email, password}=req.body
+let existingUser
+
+try{
+    existingUser=await User.findOne({email:email});
+}catch(err){
+    const error=new httpError('Your username or password are incorrect, please try again', 404);
+    return next(error);
+}
+
+if (!existingUser||existingUser.password!=password){
+    const error=new httpError('The username or password are incorrect, please try again', 404);
+    return next(error)
+}
+
+res.json({message:'Successfully logged in'})
+}
